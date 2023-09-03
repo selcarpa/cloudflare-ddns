bad28cf5adbc773ef238c5b7974c0eb82df78b1b
selcarpa <selcarpa@gmail.com>
2023-09-03 23:25:54 +0800

    update dependences

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     kotlin("multiplatform") version "1.9.0"
     id("io.ktor.plugin") version "2.3.3"
     kotlin("plugin.serialization") version "1.9.0"
-//    id("app.cash.sqldelight") version "2.0.0"
 }
 
 group = "one.tain"
@@ -17,13 +16,6 @@ repositories {
     google()
 }
 
-//sqldelight {
-//    databases {
-//        create("Database") {
-//            packageName.set("one.tain.lurker")
-//        }
-//    }
-//}
 
 kotlin {
     val hostOs = System.getProperty("os.name")
@@ -53,7 +45,6 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
                 implementation("com.squareup.okio:okio:$okio_version")
                 implementation("net.peanuuutz:tomlkt:0.2.0")
-//                implementation("app.cash.sqldelight:native-driver:2.0.0")
                 implementation("io.github.oshai:kotlin-logging-linuxx64:5.1.0")
             }
         }
