a7ad9de171ac6f059a6e2c6df4c288b92ebe3ca5
selcarpa <selcarpa@gmail.com>
2023-09-04 00:22:23 +0800

    add jvm mirror

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 val ktor_version: String by project
 val kotlin_version: String by project
-val okio_version: String by project
+val okio_version:String by project
+val logback_version: String by project
 
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     id("io.ktor.plugin") version "2.3.3"
     kotlin("plugin.serialization") version "1.9.0"
 }
@@ -16,38 +17,18 @@ repositories {
     google()
 }
 
+application {
+    mainClass.set("Main")
 
-kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
-            }
-        }
-    }
-    sourceSets {
-        commonMain {
-            dependencies {
-                implementation("io.ktor:ktor-client-core:$ktor_version")
-                implementation("io.ktor:ktor-client-cio:$ktor_version")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-                implementation("com.squareup.okio:okio:$okio_version")
-                implementation("net.peanuuutz:tomlkt:0.2.0")
-                implementation("io.github.oshai:kotlin-logging-linuxx64:5.1.0")
-            }
-        }
-
-    }
+dependencies {
+    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-cio-jvm:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.squareup.okio:okio:$okio_version")
+    implementation("net.peanuuutz:tomlkt:0.2.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
 }
