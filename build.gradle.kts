a6aa0e6e6ff981d543eec1e950bcc16f62f9b2dc
selcarpa <selcarpa@gmail.com>
2023-09-06 11:11:58 +0800

    bump kotlin multiplatform version to 1.9.10; add multiplatform build support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.*
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val okio_version: String by project
 
 plugins {
-    kotlin("multiplatform") version "1.9.0"
+    kotlin("multiplatform") version "1.9.10"
     id("io.ktor.plugin") version "2.3.3"
     kotlin("plugin.serialization") version "1.9.0"
 }
@@ -18,26 +20,36 @@ repositories {
 
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
-    nativeTarget.apply {
+    fun KotlinNativeTarget.config(custom: Executable.() -> Unit = {}) {
         binaries {
             executable {
                 entryPoint = "main"
+                custom()
             }
         }
     }
+
+    linuxX64 {
+        config()
+    }
+//    linuxArm64 {
+//        config()
+//    }
+
+
     sourceSets {
+        linuxX64 {
+            dependencies {
+                implementation("io.github.oshai:kotlin-logging-linuxx64:5.1.0")
+            }
+        }
+//        linuxArm64 {
+//            dependencies {
+//                implementation("io.github.oshai:kotlin-logging-linuxarm64:5.1.0")
+//            }
+//        }
+
         commonMain {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktor_version")
@@ -48,7 +60,6 @@ kotlin {
                 implementation("com.squareup.okio:okio:$okio_version")
                 implementation("net.mamoe.yamlkt:yamlkt:0.13.0")
                 implementation("net.peanuuutz:tomlkt:0.2.0")
-                implementation("io.github.oshai:kotlin-logging-linuxx64:5.1.0")
                 implementation("io.ktor:ktor-client-logging:$ktor_version")
             }
         }
