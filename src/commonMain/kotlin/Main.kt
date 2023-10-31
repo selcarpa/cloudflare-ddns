63de5f0f2db4e9caaec3da7244b36afe1b5e5fac
selcarpa <selcarpa@gmail.com>
2023-10-31 17:36:33 +0800

    improve code

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -54,6 +54,9 @@ expect fun debugLogSet()
 expect fun exitGracefully()
 
 
+/**
+ * main function
+ */
 fun main(args: Array<String>) = runBlocking {
     try {
         args.forEach {
@@ -95,12 +98,12 @@ private fun CoroutineScope.mainTask() {
         d.filter { it.domain.properties?.v4 ?: false }.groupBy {
             it.domain.properties!!.checkUrlV4!!
         }.forEach {
-            launchTask(ttl, it)
+            launchMainTask(ttl, it)
         }
         d.filter { it.domain.properties?.v6 ?: false }.groupBy {
             it.domain.properties!!.checkUrlV6!!
         }.forEach {
-            launchTask(ttl, it)
+            launchMainTask(ttl, it)
         }
     }
 
@@ -123,31 +126,49 @@ private fun CoroutineScope.mainTask() {
 }
 
 /**
+ * launch main ddns task
+ */
+private fun CoroutineScope.launchMainTask(
+    ttl: Int?, it: Map.Entry<String, List<DdnsItem>>
+) {
+    launch(Dispatchers.Default) {
+        delayCall(ttl!!.seconds) {
+            ddns(it.value) {
+                getIp(it.key)
+            }
+        }
+    }
+}
+
+/**
  * purge task
  * @param loopExec if true, it will loop to purge until purge successful
  */
 fun DdnsItem.purge(loopExec: Boolean = false, reason: String) = runBlocking {
     do {
         if (!this@purge.inited && !this@purge.init()) {
-            logger.error { "purge ${this@purge.domain.name} ${this@purge.type.name} failed, try after ${this@purge.domain.properties!!.ttl}" }
+            logger.error { "purge ${this@purge.domain.name} ${this@purge.type} failed, try after ${this@purge.domain.properties!!.ttl}" }
         }
 
         if (!this@purge.exists) {
-            logger.info { "no exists record for ${this@purge.domain.name} ${this@purge.type.name}, purge task stopped" }
+            logger.info { "no exists record for ${this@purge.domain.name} ${this@purge.type}, purge task stopped" }
             break
         }
-        logger.info { "start purge ${this@purge.domain.name} ${this@purge.type.name} cause: $reason" }
-        if (purge(this@purge)) {
+        logger.info { "start purge ${this@purge.domain.name} ${this@purge.type} cause: $reason" }
+        if (this@purge.doPurge()) {
             break
         }
         delay(this@purge.domain.properties!!.ttl!!.seconds)
     } while (loopExec)
 }
 
-suspend fun purge(ddnsItem: DdnsItem): Boolean {
-    val authHeader = ddnsItem.authHeader()
+/**
+ * invoke cloudflare api to purge dns record
+ */
+suspend fun DdnsItem.doPurge(): Boolean {
+    val authHeader = this.authHeader()
     val httpResponse = client.delete(
-        "https://api.cloudflare.com/client/v4/zones/${ddnsItem.domain.properties!!.zoneId}/dns_records/${ddnsItem.id}/"
+        "https://api.cloudflare.com/client/v4/zones/${this.domain.properties!!.zoneId}/dns_records/${this.id}/"
     ) {
         headers {
             authHeader.forEach {
@@ -161,22 +182,14 @@ suspend fun purge(ddnsItem: DdnsItem): Boolean {
         logger.error { cloudflareBody.errors }
         return false
     }
-    logger.info { "purge ${ddnsItem.domain.name} ${ddnsItem.type.name} successful" }
+    logger.info { "purge ${this.domain.name} ${this.type} successful" }
     return true
 }
 
-private fun CoroutineScope.launchTask(
-    ttl: Int?, it: Map.Entry<String, List<DdnsItem>>
-) {
-    launch(Dispatchers.Default) {
-        delayCall(ttl!!.seconds) {
-            ddns(it.value) {
-                getIp(it.key)
-            }
-        }
-    }
-}
 
+/**
+ * delay and loop to call exec function
+ */
 fun delayCall(duration: Duration, exec: () -> Unit) = runBlocking {
     while (true) {
         //https://kotlinlang.org/docs/coroutines-and-channels.html#using-the-outer-scope-s-context
@@ -194,6 +207,9 @@ fun delayCall(duration: Duration, exec: () -> Unit) = runBlocking {
     }
 }
 
+/**
+ * ddns task
+ */
 private fun ddns(ddnsItems: List<DdnsItem>, ipSupplier: suspend () -> String) = runBlocking {
     logger.debug { "start ${ddnsItems[0].type} ddns task for ${(ddnsItems.joinToString(",") { it.domain.name })}" }
     runCatching {
@@ -209,7 +225,7 @@ private fun ddns(ddnsItems: List<DdnsItem>, ipSupplier: suspend () -> String) = 
             logger.error { e.message }
         }
         ddnsItems.forEach {
-            logger.warn { "ddns task for ${it.domain.name} ${it.type.name} failed, try after ${it.domain.properties!!.ttl}" }
+            logger.warn { "ddns task for ${it.domain.name} ${it.type} failed, try after ${it.domain.properties!!.ttl}" }
         }
         ddnsItems.filter { it.domain.properties?.autoPurge!! }.forEach { ddnsItem ->
             ddnsItem.purge(reason = "get ip failed, so purge it")
@@ -217,6 +233,10 @@ private fun ddns(ddnsItems: List<DdnsItem>, ipSupplier: suspend () -> String) = 
     }
 }
 
+/**
+ * get current ip by checkApi
+ * @param checkApi checkApi url
+ */
 suspend fun getIp(checkApi: String): String {
     val response = client.get(checkApi)
     logger.debug { response }
@@ -225,6 +245,9 @@ suspend fun getIp(checkApi: String): String {
     return ip
 }
 
+/**
+ * convert domain to ddns item
+ */
 private fun Domain.toV4DdnsItems(): DdnsItem {
     return DdnsItem(
         domain = this, type = TYPE.A
@@ -237,6 +260,9 @@ private fun Domain.toV6DdnsItems(): DdnsItem {
     )
 }
 
+/**
+ * convert domain to ddns item list
+ */
 private fun Domain.toDDnsItems(): List<DdnsItem> {
     return listOfNotNull(
         if (this.properties!!.v4!!) {
@@ -252,6 +278,10 @@ private fun Domain.toDDnsItems(): List<DdnsItem> {
     )
 }
 
+/**
+ * run ddns task, include init task, detect if need to create or update dns record
+ * @param ip current ip
+ */
 private fun DdnsItem.run(ip: String) = runBlocking {
     if (!this@run.inited && !this@run.init()) {
         return@runBlocking
@@ -266,28 +296,34 @@ private fun DdnsItem.run(ip: String) = runBlocking {
                 "old record for ${this@run.domain.name} is $ip ${proxiedString(this@run.proxied!!)} (${this@run.ttl}), target is ${this@run.content} ${this@run.domain.properties!!.proxied} (${this@run.domain.properties!!.ttl})"
             }
             logger.info {
-                "update ${this@run.domain.name} to ${this@run.type.name} $ip ${proxiedString(this@run.domain.properties!!.proxied!!)} (${this@run.domain.properties!!.ttl})"
+                "update ${this@run.domain.name} to ${this@run.type} $ip ${proxiedString(this@run.domain.properties!!.proxied!!)} (${this@run.domain.properties!!.ttl})"
             }
             updateDns(ip, this@run, true)
             return@runBlocking
         }
 
         logger.info {
-            "checked: ${this@run.type} ${this@run.domain.name} already been resolve to $ip ${proxiedString(this@run.domain.properties!!.proxied!!)}"
+            "checked: ${this@run.domain.name} ${this@run.type} already been resolve to $ip ${proxiedString(this@run.domain.properties!!.proxied!!)}"
         }
         return@runBlocking
     } else {
         logger.info {
-            "create ${this@run.domain.name} with ${this@run.type.name} $ip ${proxiedString(this@run.domain.properties!!.proxied!!)}"
+            "create ${this@run.domain.name} ${this@run.type} with $ip ${proxiedString(this@run.domain.properties!!.proxied!!)}"
         }
         updateDns(ip, this@run)
     }
 }
 
+/**
+ * get proxied string
+ */
 private fun proxiedString(proxied: Boolean) = if (proxied) "not proxied" else {
     "proxied"
 }
 
+/**
+ * invoke cloudflare api to create or update dns record
+ */
 suspend fun updateDns(ip: String, ddnsItem: DdnsItem, update: Boolean = false) {
     val authHeader = ddnsItem.authHeader()
     val httpResponse = client.request(
@@ -332,6 +368,9 @@ suspend fun updateDns(ip: String, ddnsItem: DdnsItem, update: Boolean = false) {
     ddnsItem.init(cloudflareBody.result!!)
 }
 
+/**
+ * invoke cloudflare api to get current dns record
+ */
 private suspend fun DdnsItem.init(): Boolean {
     val authHeader = this.authHeader()
     val dnsRecords =
@@ -346,9 +385,9 @@ private suspend fun DdnsItem.init(): Boolean {
                     "name",
                     this@init.domain.name,
                 )
-                this@init.type.name.let {
+                this@init.type.let {
                     parameters.append(
-                        "type", it
+                        "type", it.name
                     )
                 }
             }
@@ -363,12 +402,15 @@ private suspend fun DdnsItem.init(): Boolean {
         init(cloudflareBody.result!!.first())
         true
     } else {
-        logger.info { "no ${this@init.type.name} exists record found for ${this@init.domain.name}" }
+        logger.info { "no exists ${this@init.domain.name} ${this@init.type} record found" }
         true
     }
 
 }
 
+/**
+ * init DdnsItem by cloudflare api result
+ */
 private fun DdnsItem.init(dnsRecord: DnsRecord) {
     logger.debug { "exists dnsRecord: $dnsRecord" }
     this.ttl = dnsRecord.ttl
@@ -379,6 +421,9 @@ private fun DdnsItem.init(dnsRecord: DnsRecord) {
     this.exists = true
 }
 
+/**
+ * get auth header
+ */
 private fun DdnsItem.authHeader(): Map<String, String> {
     return mapOf(
         Pair("Authorization", "Bearer ${this.domain.properties!!.authKey!!}")
