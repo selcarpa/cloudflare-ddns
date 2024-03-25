44e457935425b6488c991cb0d1045714a8e48f03
selcarpa <selcarpa@gmail.com>
2024-03-25 15:31:28 +0800

    Fix debug log set

--- src/nativeMain/kotlin/Main.native.kt
+++ src/nativeMain/kotlin/Main.native.kt
@@ -11,7 +11,7 @@ import platform.posix.stderr
 import kotlin.system.exitProcess
 
 actual fun debugLogSet() {
-    KotlinLoggingConfiguration.logLevel = Level.ERROR
+    KotlinLoggingConfiguration.logLevel = Level.DEBUG
 }
 
 actual fun exitGracefully() {
